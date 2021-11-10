f919ec1a11c7592d31e428f0b144e6209c2409d5
LAPTOP-UE4NHFS6\armin <armindjulovic@msn.com>
2021-11-10 22:32:26 +0100

    hej

---
+++ app.js
@@ -0,0 +1,29 @@
+"use strict";
+// let imageElement = document.querySelector("#unsplashImage");
+
+// fetch("./images.json")
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (jsonData) {
+//     console.log(jsonData);
+//     imageElement.src = jsonData.url;
+//   });
+// console.log(imageElement);
+
+/*
+let endpoint = "./images.json";
+
+let imageLink = document.querySelector("#imageLink");
+let imageElement = document.querySelector("#unsplashImage");
+
+fetch(endpoint)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonData) {
+    console.log(jsonData);
+    imageElement.src = jsonData.url;
+  });
+console.log(imageElement);
+*/
